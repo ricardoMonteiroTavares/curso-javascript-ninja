fb3303f342e10b1c55400ec909886c2e216535e7
Ricardo de Monteiro e Tavares <ricardotavares51@hotmail.com>
2021-08-13 12:12:52 -0300

    Update challenge-13.js

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -74,31 +74,35 @@ Crie um novo array chamado `nordeste`, que tenha os estados do nordeste.
 Mostre no console os estados do nordeste.
 */
 console.log( '\nEstados do Nordeste:' );
-// ?
+  console.log(nordeste);
+// ["BA", "PE", "PB", "SE", "AL", "CE", "PI", "RN", "MA"]
 
 /*
 Remova de `brasil` os estados do `sudeste`, colocando-os em uma variável
 chamada `newSudeste`.
 */
-// ?
+  var newSudeste = brasil.splice(5,);
 
 /*
 Adicione os estados do `nordeste` ao array `brasil`. Esses estados devem
 ficar no mesmo nível que os estados já existentes, não em um array separado.
 */
+  brasil = brasil.concat(nordeste);
 // ?
 
 /*
 Mostre no console os estados em `newSudeste`.
 */
 console.log( '\nEstados em newSudeste:' );
-// ?
+console.log(newSudeste);
+  //  ["ES", "MG", "RJ", "SP"]
 
 /*
 Mostre no console os estados do `brasil`.
 */
 console.log( '\nAlguns estados do Brasil:' );
-// ?
+  console.log(brasil);
+// ["AM", "AP", "PR", "RS", "SC", "BA", "PE", "PB", "SE", "AL", "CE", "PI", "RN", "MA"]
 
 /*
 usando forEach, percorra o array `brasil` e gere um novo array chamado
@@ -107,13 +111,34 @@ propriedades:
 - `id`: que será o índice do array `brasil`,
 - `estado`: que será o estado do array `brasil`.
 */
-// ?
+  var newBrasil = [];
+brasil.forEach(function(val, index){
+    newBrasil.push({id: index, estado: val});
+});
 
 /*
 Mostre o array `newBrasil` no console
 */
 console.log( '\nnewBrasil:' );
-// ?
+  console.log(newBrasil);
+/*
+[
+ {id: 0, estado: "AM"},
+ {id: 1, estado: "AP"},
+ {id: 2, estado: "PR"},
+ {id: 3, estado: "RS"},
+ {id: 4, estado: "SC"},
+ {id: 5, estado: "BA"},
+ {id: 6, estado: "PE"},
+ {id: 7, estado: "PB"},
+ {id: 8, estado: "SE"},
+ {id: 9, estado: "AL"},
+ {id: 10, estado: "CE"},
+ {id: 11, estado: "PI"},
+ {id: 12, estado: "RN"},
+ {id: 13, estado: "MA"}
+]
+*/
 
 /*
 Percorra o array `brasil` e verifique se os estados tem mais de 7 letras cada,
@@ -154,11 +179,25 @@ console.log( '\nnewBrasil agora com mais informações:' );
 Filtre o array criado acima, retornando somente os estados que tiverem
 ID par. Atribua o valor à uma variável chamada `filter`.
 */
-// ?
-
+var filter = newBrasil.filter(function(elem){
+    if(elem["id"] % 2 == 0){
+        return elem;
+    }
+});
 /*
 Mostre o array filtrado acima no console.
 */
 console.log( '\nEstados com ID par:' );
-// ?
+  console.log(filter);
+/*
+[
+ {id: 0, estado: "AM"},
+ {id: 2, estado: "PR"},
+ {id: 4, estado: "SC"},
+ {id: 6, estado: "PE"},
+ {id: 8, estado: "SE"},
+ {id: 10, estado: "CE"},
+ {id: 12, estado: "RN"}
+ ]
+*/
 })();
