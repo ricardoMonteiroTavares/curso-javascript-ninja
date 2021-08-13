573f14ab4f8531979e9a77287771bbd6de5d7a91
Ricardo de Monteiro e Tavares <ricardotavares51@hotmail.com>
2021-08-12 23:41:53 -0300

    Exercício 12

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,3 +1,4 @@
+(function(){
 /*
 Envolva todo o conteúdo desse arquivo em uma IIFE.
 */
@@ -10,19 +11,25 @@ Crie um objeto chamado `person`, com as propriedades:
 Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
 de valor para cada propriedade.
 */
-// ?
+var person = {
+  name: "Ricardo",
+  lastname: "Tavares",
+  age: 24,  
+};
 console.log( 'Propriedades de "person":' );
 
 /*
 Mostre no console, em um array, todas as propriedades do objeto acima.
 Não use nenhuma estrutura de repetição, nem crie o array manualmente.
 */
-// ?
+ console.log(Object.keys(person));
+// Propriedades de "person":
+// (3) ["name", "lastname", "age"]
 
 /*
 Crie um array vazio chamado `books`.
 */
-// ?
+var books = [];
 
 /*
 Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
@@ -30,19 +37,38 @@ seguintes propriedades:
 `name`: String
 `pages`: Number
 */
-// ?
+books.push({
+  name: "A arte de ligar o foda-se",
+  pages: 245
+});
+books.push({
+  name: "Como as democracias morrem",
+  pages: 531
+});
+books.push({
+  name: "Amanhã vai ser pior",
+  pages: 217
+});
 console.log( '\nLista de livros:' );
 
 /*
 Mostre no console todos os livros.
 */
-// ?
+  console.log(books);
+/*
+[
+{name: "A arte de ligar o foda-se", pages: 245},
+{name: "Como as democracias morrem", pages: 531},
+{name: "Amanhã vai ser pior", pages: 217}
+]
+*/
 
 console.log( '\nLivro que está sendo removido:' );
 /*
 Remova o último livro, e mostre-o no console.
 */
-// ?
+  console.log(books.pop());
+// {name: "Amanhã vai ser pior", pages: 217}
 
 console.log( '\nAgora sobraram somente os livros:' );
 /*
@@ -59,7 +85,10 @@ console.log( '\nLivros em formato string:' );
 /*
 Mostre os livros nesse formato no console:
 */
-// ?
+console.log(JSON.stringify(books));
+/*
+[{"name":"A arte de ligar o foda-se","pages":245},{"name":"Como as democracias morrem","pages":531}]
+*/
 
 /*
 Converta os livros novamente para objeto.
@@ -72,29 +101,37 @@ Mostre no console todas as propriedades e valores de todos os livros,
 no formato abaixo:
     "[PROPRIEDADE]: [VALOR]"
 */
-// ?
+for (var i = 0; i < books.length; i++) {
+    for (var prop in books[i]) {
+      console.log(prop + ': ' + books[i][prop]);
+    }
+  }
 
 /*
 Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
 seu nome. Adicione seu nome completo no array.
 */
-// ?
+var myName = ['R','i','c','a','r','d','o'];  
 console.log( '\nMeu nome é:' );
 
 /*
 Juntando todos os itens do array, mostre no console seu nome.
 */
-// ?
+  console.log(myName.join(""));
+// Ricardo
 
 console.log( '\nMeu nome invertido é:' );
 
 /*
 Ainda usando o objeto acima, mostre no console seu nome invertido.
 */
-// ?
+  console.log(myName.reverse().join(""));
+// odraciR
 
 console.log( '\nAgora em ordem alfabética:' );
 /*
 Mostre todos os itens do array acima, odenados alfabéticamente.
 */
-// ?
+  console.log(myName.sort().join(""));
+// Racdior
+})();;
