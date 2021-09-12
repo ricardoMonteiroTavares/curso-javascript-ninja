de74701efbf59b84d784d4c4c89a6b3a78b6b5c3
Ricardo Tavares <ricardotavares51@hotmail.com>
2021-09-12 18:51:00 -0300

    Exercício 21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,3 +1,6 @@
+(function(win, doc){
+    'use strict';
+
 /*
 O desafio de hoje será um pequeno projeto: um cronômetro!
 As regras para criação do cronômetro são as seguintes:
@@ -14,4 +17,47 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+var counter = 0;
+var idInterval = -1;
+var $startBtn = doc.querySelector('input[crType="start"]');
+var $stopBtn = doc.querySelector('input[crType="stop"]');
+var $resetBtn = doc.querySelector('input[crType="reset"]');
+var $display = doc.querySelector('h3[crType="display"]');
+$display.innerHTML = format(counter);
+$startBtn.addEventListener("click", start);
+$stopBtn.addEventListener("click", stop);
+$resetBtn.addEventListener("click", reset);
+
+function start(event){
+    event.preventDefault();
+    if(idInterval == -1){
+        idInterval = win.setInterval(add, 1000);
+        console.log("Iniciou");
+    }
+    
+}
+
+function stop(event){
+    event.preventDefault();
+    if(idInterval != -1){
+        win.clearInterval(idInterval);
+        idInterval = -1;
+        console.log("Parou");
+    }
+    
+}
+
+function reset(event){
+    event.preventDefault();
+    counter = 0;
+    $display.innerHTML = format(counter);
+    console.log("Resetou")
+}
+
+function add(){
+    $display.innerHTML = format(++counter);
+}
+
+function format(val) { return val + " seg";}
+
+})(window, document);
